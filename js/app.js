14a5e1e874b68cc25db5b5987033ad7cd1219c8b
GodCousin <vanistaochieng@gmail.com>
2025-12-20 23:01:00 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,7 @@
+const APP_ID = "112117";
 
+function loginDeriv() {
+  const redirect = "https://god-cousin-trading-hub.vercel.app/dashboard.html";
+  const url = `https://oauth.deriv.com/oauth2/authorize?app_id=${APP_ID}&redirect_uri=${redirect}`;
+  window.location.href = url;
+}
